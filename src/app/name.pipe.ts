c81b54803d6b09a3b26c1c10a4dbc50371a43021
Anuroop <anuroopjoy@gmail.com>
2022-05-15 22:45:09 +0530

    pipe

--- src/app/name.pipe.ts
+++ src/app/name.pipe.ts
@@ -2,6 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'name',
+  // pure: false
 })
 export class NamePipe implements PipeTransform {
   transform(value: unknown, ...args: unknown[]): unknown {
